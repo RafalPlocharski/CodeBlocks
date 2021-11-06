70908116f8c09377c80c6a572232dd6d71e577ef
RafalPlocharski <93700919+RafalPlocharski@users.noreply.github.com>
2021-11-06 11:28:39 +0100

    Add files via upload

---
+++ 2 rozdzial 2 zadanie.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Rafal Plocharski \n");
+    printf("Rafal \nPlocharski \n");
+    printf("Rafal");
+    printf(" Plocharski");
+
+
+    return 0;
+}
