bcd9fdba96c8e7eb36b6c8a7a670b424e0f0499d
RafalPlocharski <93700919+RafalPlocharski@users.noreply.github.com>
2021-11-11 20:20:33 +0100

    Add files via upload

---
+++ 2 rozdzial 6 zadanie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void wolaj_a(void);
+
+int main(void)
+{
+  wolaj_a(); wolaj_a(); wolaj_a(); printf("\n");
+  wolaj_a(); wolaj_a(); printf("\n");
+  wolaj_a();
+
+  return 0;
+}
+
+
+void wolaj_a(void)
+{
+  printf("Usmiech!");
+}
