920e51f144991398818d1008d6a3ff102e48a7bf
RafalPlocharski <93700919+RafalPlocharski@users.noreply.github.com>
2021-11-11 21:22:22 +0100

    Add files via upload

---
+++ 3 rozdzial 3 zadanie.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    Beep(500,1000);
+    printf("Sally, przerazona niespodziewanym odglosem, krzyknela \"A niech mnie, co to bylo!?\"");
+    return 0;
+}
