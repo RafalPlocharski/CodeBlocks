a76bcdda970c6aa8c9099505f5262c42440ccc57
RafalPlocharski <93700919+RafalPlocharski@users.noreply.github.com>
2021-11-11 21:06:43 +0100

    Add files via upload

---
+++ 3 rozdzial 2 zadanie.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ASCII;
+    printf("Prosze wprowadzic kod ASCII: ");
+    scanf("%i", &ASCII);
+    printf("%c \n", ASCII);
+    return 0;
+}
